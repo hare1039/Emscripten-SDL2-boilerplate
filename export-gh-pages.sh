fcd3422963affc461d452934eb054991d3dbe190
hare1039 <hare39393@gmail.com>
2019-04-20 15:23:03 +0800

    Add asset into export-gh-pages.sh

--- export-gh-pages.sh
+++ export-gh-pages.sh
@@ -4,9 +4,10 @@ GAME_ROOT=$(git rev-parse --show-toplevel)
 
 cd ${GAME_ROOT}
 git checkout gh-pages
-git checkout master -- src/
+git checkout master -- src/ asset/
 cd src
+make clean
 make
 cd ..
-git reset HEAD src
+git reset HEAD src asset
 echo "Now, please add and commit to your gh-pages"
