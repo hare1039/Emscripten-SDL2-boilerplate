f2a5a27eead2d18dbe529b18769dee00dc2f5ec7
hare1039 <hare39393@gmail.com>
2019-05-14 19:07:23 +0800

    Change SDL_GetTicks() to std::chrono

--- src/animation.hpp
+++ src/animation.hpp
@@ -3,48 +3,56 @@
 
 #pragma once
 
+#include <chrono>
 #include "basic_headers.hpp"
 
 namespace game
 {
+
+namespace {
+    using namespace std::chrono_literals;
+}
 
 class animation
 {
 private:
     int  current_frame      = 0;
     int  frame_inc          = 1;
-    unsigned long old_time  = 0;
+    std::chrono::high_resolution_clock::time_point old_time;
     bool frame_locked = false;
 
+    static inline auto now() { return std::chrono::high_resolution_clock::now(); }
+
 public:
-    int  frame_rate    = 100; //Milliseconds
-    int  max_frame     = 1;
+    std::chrono::high_resolution_clock::duration frame_rate  = 100ms;
+    int  max_frame    = 1;
 
     enum rotate_type
     {
         none,
         circle,
         oscillate
-    } rotate_type_id = none;
+    };
+    rotate_type rotate_type_ = none;
 
 public:
-    animation(int max_frame, rotate_type t): max_frame{max_frame}, rotate_type_id{t} {}
+    animation(int max_frame, rotate_type t): max_frame {max_frame}, rotate_type_ {t} {}
 
     void animate()
     {
-        if (frame_locked or (old_time + frame_rate > SDL_GetTicks()))
+        if (frame_locked or (old_time + frame_rate > now()))
             return;
 
-        old_time = SDL_GetTicks();
+        old_time = now();
         current_frame += frame_inc;
 
-        if (rotate_type_id == rotate_type::oscillate)
+        if (rotate_type_ == rotate_type::oscillate)
         {
             if ((frame_inc > 0 && current_frame >= max_frame) ||
                 (current_frame <= 0))
                 frame_inc = -frame_inc;
         }
-        else if (rotate_type_id == rotate_type::circle)
+        else if (rotate_type_ == rotate_type::circle)
         {
             if (frame_inc > 0)
             {
